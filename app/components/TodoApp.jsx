2dd1112ee56e779c870938004eef821a486c40a2
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-17 10:21:33 -0700

    Create components folder and todoapp file

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var react = require('React');
+
+var TodoApp = React.createClass({
+  render function () {
+    return (
+    <div>
+      TodoApp.jsx
+    </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
