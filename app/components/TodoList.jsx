f0f7e6f9176723f8e9620d5c088dd07c536a1189
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-18 21:02:59 -0700

    Create todolist component

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoList = React.createClass({
+  render: function () {
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+});
+
+module.exports = TodoLists;
